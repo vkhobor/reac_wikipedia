0c4e0262c01c4f0e7d5fa44d6cdd7c6e39e27452
vkhobor <47790799+vkhobor@users.noreply.github.com>
2022-09-10 01:51:16 +0200

    feat: create RatingCell.tsx

---
+++ src/components/table/RatingCell.tsx
@@ -0,0 +1,20 @@
+import { Rating } from "@mui/material";
+import { FunctionComponent } from "react";
+
+interface RatingCellProps {
+  ratingOfTen: number;
+}
+
+const RatingCell: FunctionComponent<RatingCellProps> = ({ ratingOfTen }) => {
+  return (
+    <Rating
+      name="simple-controlled"
+      readOnly
+      max={10}
+      precision={0.5}
+      value={ratingOfTen}
+    />
+  );
+};
+
+export default RatingCell;
