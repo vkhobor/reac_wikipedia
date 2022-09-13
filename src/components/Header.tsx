ac255d58e98992829ede6954ae07e0cfd117843f
vkhobor <47790799+vkhobor@users.noreply.github.com>
2022-09-13 06:16:09 +0200

    conditionally show commit hash

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,17 @@ import { tokens } from "../ciTokens"
 const Header: FunctionComponent<EmptyInterface> = () => {
   return (
     <header className="sticky top-0 z-10 bg-materialBlue shadow-material h-14 p-3 flex justify-between items-center flex-row">
-      <span className="text-sm">{tokens.commitHash}</span>
+      <div className="text-sm">
+        {tokens.commitHash === "#{COMMIT_HASH}#" && (
+          <>
+            <div className="flex flex-col">
+              <span className="opacity-70 font-bold">commit</span>
+              <span>{tokens.commitHash}</span>
+            </div>
+          </>
+        )}
+      </div>
+
       <CatIcon className="hover:text-black text-white h-full w-auto" />
     </header>
   )
